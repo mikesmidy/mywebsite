c75621537d6035812de2c4843e9a9d824b505822
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-06-12 23:52:03 +0100

    fix display count error

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
     getVisitCount();
 });
 
 const apiGateway = 'https://gkt25sgaaa.execute-api.us-east-2.amazonaws.com/Prod/visitor/'; 
 
 const getVisitCount = () => {
-    console.log('api to get visit count');
-    let count = getCountApi();
-    document.getElementById('counter').innerText =count;
-}
-
-const getCountApi = () => {
     let count = 0;
     fetch(apiGateway, {
         mode: 'cors',
@@ -20,8 +13,8 @@ const getCountApi = () => {
         return response.json()
     })
     .then(res => {
-        console.log('jsn', res.Attributes.visitcount)
-        count = res.Attributes.visitcount;
+        const count = res.Attributes.visitcount;
+        document.getElementById('counter').innerText =count;
     })
     return count;
 }
